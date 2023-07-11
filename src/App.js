28e29ac0152123ca0bcb6e774abd176d755ef403
Rakshita goud <krakshitagoud@gmail.com>
2023-07-11 10:13:30 +0530

    props and state handler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Ex1 from './component/Ex1'
 import Ex2 from './component/Ex2'
 import Ex4 from './component/Ex4'
+import Ex5S from './component/Ex5'
+import Ex5 from './component/Ex5'
+import Ex6 from './component/Ex6'
 
 function App(props){
   return(
     <div>
       <h1>States in React</h1>
-      <Ex4/>    
+      <Ex6/>    
    </div>
   )
 }
